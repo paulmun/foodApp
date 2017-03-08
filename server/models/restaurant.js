aa5b3ffbd7fc5eabb2f78860460aa62a754c6df8
Paul Mun <paulmun818@gmail.com>
2017-03-07 16:12:16 -0800

    Added restaurant schema

---
+++ server/models/restaurant.js
@@ -0,0 +1,56 @@
+(function(){
+
+    var mongoose = require('mongoose'),
+        Schema = mongoose.Schema;
+
+    var ServiceReviewSchema = new Schema({
+        owner: {
+            type: Schema.types.ObjectId,
+            ref: 'User'
+        },
+
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+
+        loc: {
+            x: {
+                type: Number,
+                required: true
+            },
+            y: {
+                type: Number,
+                required: true
+            }
+        },
+
+        rating: {
+            type: Number,
+            default: 0
+        },
+
+        foodRating: {
+            type: Number,
+            default: 0,
+            totRatings: {
+                type: Number
+                default: 0
+            }
+        },
+
+        serviceRating: {
+            type: Number,
+            default: 0,
+            totRatings: {
+                type: Number
+                default: 0
+            }
+        }
+
+    });
+
+    mongoose.model('ServiceReview', ServiceReviewSchema);
+
+})();
