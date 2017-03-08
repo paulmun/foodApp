861b4083be60e600b5e0277d984a9172c8928f1d
Paul Mun <paulmun818@gmail.com>
2017-03-07 16:11:04 -0800

    Added serviceReview schema

---
+++ server/models/serviceReview.js
@@ -0,0 +1,42 @@
+(function(){
+
+    var mongoose = require('mongoose'),
+        Schema = mongoose.Schema;
+
+    var ServiceReviewSchema = new Schema({
+        user: {
+            type: Schema.types.ObjectId,
+            ref: 'User',
+            rquired: true;
+        },
+
+        ambience: {
+            type: String
+        },
+
+        title: {
+            type: String,
+            trim: true,
+            required: true
+        },
+
+        review: {
+            type: String,
+            trim: true
+        },
+
+        rating: {
+            type: Number,
+            required: true
+        },
+
+        restaurant: {
+            type: Schema.Types.objectId,
+            ref: 'Restaurant',
+            required: true
+        }
+    });
+
+    mongoose.model('ServiceReview', ServiceReviewSchema);
+
+})();
